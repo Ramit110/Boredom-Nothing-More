0ef49e53d9c945e8732803c45288e411774097e2
Ramit110 <Ramit110@users.noreply.github.com>
2017-04-27 12:42:28 +0100

    +1

--- Python/Euler/Problem_6.py
+++ Python/Euler/Problem_6.py
@@ -2,10 +2,10 @@ Mathses = __import__("Euler_Lib")
 
 def Chal6(limit):# returns the difference of the sum and squares of all natural numbers until 'limit'
     sqr, tot = 0, 0
-    for x in range(1, limit):
+    for x in range(1, limit+1):
         sqr+=int(Mathses.Powerify(x, 2))
         tot+=int(x)
     tot=int(Mathses.Powerify(tot, 2))
-    return (int(tot)-int(sqr))
+    return(int(tot)-int(sqr))
 
 print(Chal6(100))
