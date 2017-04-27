e2f959dd5e72b34bd9793893a9abefd2b1a0efc5
Ramit110 <Ramit110@users.noreply.github.com>
2017-04-27 12:39:53 +0100

    Works now...

--- Python/Euler/Problem_4.py
+++ Python/Euler/Problem_4.py
@@ -3,8 +3,11 @@ Mathses = __import__("Euler_Lib")
 def Chal4(limit):# Finds the largest two multiples that can be made up to 'limit'
     for x in range(1, limit):
         if(Mathses.IfPalindome((limit-x)*(limit)) == True):
-            return limit-x
+            return limit*(limit-x)
+    return 0
 
-
-
-print(Chal4(99))
+out = 0
+for x in range(999):
+    if(Chal4(x) >= out):
+        out=Chal4(x)
+print(out)
