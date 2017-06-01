d4222de12fd751cbca161e6e1b204463852013ce
Ramit110 <Ramit110@users.noreply.github.com>
2017-06-01 13:41:32 +0100

    Update 0004_TwosComplement.py

--- Python/Personal_Projects/0004_TwosComplement.py
+++ Python/Personal_Projects/0004_TwosComplement.py
@@ -5,9 +5,8 @@ def TwosComplement(Num):
     for x in range(0, len(Num)):
         if(Num[x] == "0"):
             out += "1"
-        elif(Num[x] == "1"):
+        elSE:
             out += "0"
-    out = AddBin.AddBinary(out, ((len(out)-1)*"0")+"1")
-    return out
+    return AddBin.AddBinary(out, ((len(out)-1)*"0")+"1")
 
 print(TwosComplement(input("Enter Binary: ")))
